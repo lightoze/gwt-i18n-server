e539b14a9ee2a679a4c8a6c9c3058bbce92873c5
Vladimir Kulev <vladimir.kulev@teklabs.com>
2015-11-19 18:00:55 +0200

    Ignore invalid locales in I18nFilter (fixes #21)

--- src/main/java/net/lightoze/gwt/i18n/server/I18nFilter.java
+++ src/main/java/net/lightoze/gwt/i18n/server/I18nFilter.java
@@ -1,6 +1,7 @@
 package net.lightoze.gwt.i18n.server;
 
 import org.apache.commons.lang3.LocaleUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.*;
 import javax.servlet.http.Cookie;
@@ -18,10 +19,21 @@ public class I18nFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
+    private static Locale toLocale(String locale) {
+        if (StringUtils.isBlank(locale)) {
+            return null;
+        }
+        try {
+            return LocaleUtils.toLocale(locale);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
-        Locale locale = req.getMethod().equals("GET") ? LocaleUtils.toLocale(servletRequest.getParameter("locale")) : null;
+        Locale locale = req.getMethod().equals("GET") ? toLocale(servletRequest.getParameter("locale")) : null;
         if (locale != null) {
             req.getSession(true).setAttribute("locale", locale);
         } else {
@@ -37,7 +49,7 @@ public class I18nFilter implements Filter {
             if (cookies != null) {
                 for (Cookie curr : cookies) {
                     if (curr.getName().equals("locale")) {
-                        locale = LocaleUtils.toLocale(curr.getValue());
+                        locale = toLocale(curr.getValue());
                         break;
                     }
                 }
